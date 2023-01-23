489a6484ad1259bd3bc9f3221070589e1e457367
cityowl / jigmet <cityowl@hey.com>
2023-01-23 19:15:46 +0530

    webview

--- app/src/main/java/world/wildfire/app/MainActivity.kt
+++ app/src/main/java/world/wildfire/app/MainActivity.kt
@@ -2,10 +2,34 @@ package world.wildfire.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.loadUrl("https://tribe.wildfire.world")
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.webViewClient = WebViewClient()
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        val myWebView: WebView = findViewById(R.id.webview)
+
+        // Check if the key event was the Back button and if there's history
+        if (keyCode == KeyEvent.KEYCODE_BACK && myWebView.canGoBack()) {
+            myWebView.goBack()
+            return true
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event)
     }
 }
