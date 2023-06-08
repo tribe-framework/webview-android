6429d6d33be46a5b94c168e24a27221c22459c08
cityowl / jigmet <cityowl@hey.com>
2023-06-08 17:19:31 +0530

    configure for notifications

--- app/src/main/java/world/wildfire/app/MainActivity.kt
+++ app/src/main/java/world/wildfire/app/MainActivity.kt
@@ -1,22 +1,41 @@
 package world.wildfire.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
+import android.webkit.CookieManager
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import com.onesignal.OneSignal
+
+const val ONESIGNAL_APP_ID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+var versionCode = BuildConfig.VERSION_CODE
+var versionName = BuildConfig.VERSION_NAME
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         supportActionBar?.hide()
-
         val myWebView: WebView = findViewById(R.id.webview)
-        myWebView.loadUrl("https://tribe.wildfire.world")
+
+        // Cookies
+        CookieManager.getInstance().setAcceptThirdPartyCookies(myWebView, true)
+        CookieManager.getInstance().setAcceptCookie(true);
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this)
+        OneSignal.setAppId(ONESIGNAL_APP_ID)
+        OneSignal.promptForPushNotifications()
+        val device = OneSignal.getDeviceState()
+        val playerId = device!!.userId
+
         myWebView.settings.javaScriptEnabled = true
+        myWebView.settings.javaScriptCanOpenWindowsAutomatically = true
+
+        myWebView.loadUrl("https://tribe.wildfire.world/?os=android&player_id=$playerId&version_name=$versionName&version_code=$versionCode")
+
         myWebView.webViewClient = WebViewClient()
     }
 
